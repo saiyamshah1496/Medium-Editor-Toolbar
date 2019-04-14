84c2cd0d111923e510d8f01c4a99ee14891f2a4c
Saiyam Shah <saiyamshah1496@gmail.com>
2019-04-14 17:02:15 -0400

    Added all the files

---
+++ blog/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from blog.models import Post,Comment
+
+class PostForm(forms.ModelForm):
+
+    class Meta():
+        model = Post
+        fields = ('author','title','text')
+
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-insert-plugin postcontent'})
+        }
+
+class CommentForm(forms.ModelForm):
+
+    class Meta():
+        model = Post
+        fields=('text',)
+
+        widgets = {
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-insert-plugin postcontent'})
+        }
